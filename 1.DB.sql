ff920525cfc619e4edd34bb4ef4c96b72efb806e
Rajat Singh Tomar <67816280+rajat290@users.noreply.github.com>
2024-12-19 11:36:49 +0530

    Add files via upload

---
+++ 1.DB.sql
@@ -0,0 +1,36 @@
+What is a Database, and Why is it Used?
+
+A Database is way to Store and organize the data and collections of data that can be easily managed,access and UPDATE
+
+DBMS can be classified into two main types:
+
+Relational Database Management System (RDBMS)
+Non-Relational Database Management System (NoSQL or Non-SQL)
+
+in SQL, What is a database?
+
+A database in SQL is an organized collection of Structured data. Databases help us with efficiently storing, accessing, and manipulating data held on a computer system or Server.
+
+Example: A school management system database stores data about students, teachers, and classes.
+
+
+Basic SQL Concepts
+
+1. Tables: A collection of related data in rows and columns.
+Example:
+
+ID	Name	Age
+1	Rajat	23
+2	Virat	23
+
+2. Rows :  a row is the underlying logical grouping of related data in a table
+Example: (1, 'Rajat', 23) is one row.
+
+3. Columns:it is Attributes of the table.
+Example: ID, Name, and Age are columns.
+
+4. Primary Key: A unique identifier for each row in a table.
+Example: ID is the primary key in the above table.
+
+5. Foreign Key: A field in one table that refers to the primary key of another table to create a relationship.
+Example: In a Students table, ClassID can be a foreign key referring to the ID in a Classes table.
